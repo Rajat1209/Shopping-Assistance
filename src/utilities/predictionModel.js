9c211dcc06906921917099715c4f5569fe6d2ded
nikhilg225 <nikhil.gupta.225@gmail.com>
2022-05-29 21:19:01 +0530

    Changed database URL.

--- src/utilities/predictionModel.js
+++ src/utilities/predictionModel.js
@@ -16,7 +16,7 @@ const predictionModel = () => {
   const saveAndGetObject=async (image,setIsPrediction)=>{
     const [metadata,base64Image] = image.split(",");
     await fetch(
-        "https://shopping-assistance-final-viva-default-rtdb.firebaseio.com/Image.json",
+        "https://shopping-assistance-c964c-default-rtdb.firebaseio.com/Image.json",
         {
             method: 'POST',
             body: JSON.stringify({"imageData":base64Image}),
